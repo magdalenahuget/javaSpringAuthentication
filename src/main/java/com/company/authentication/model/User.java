5e5ba041c88ecda824f45f0440a4fdd84c3b5df4
magdalenahuget <magdalena.huget@gmail.com>
2023-10-18 13:45:06 +0200

    Add jakarta validation dependency and comment to user model

--- src/main/java/com/company/authentication/model/User.java
+++ src/main/java/com/company/authentication/model/User.java
@@ -1,16 +1,14 @@
 package com.company.authentication.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.util.HashSet;
 import java.util.Set;
 
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+
+
 @Entity
 @Table(name = "users",
         uniqueConstraints = {
@@ -36,10 +34,37 @@ public class User {
     private String password;
 
     @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "user_roles",
+    @JoinTable(  name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
+
+
+    /*
+   @ManyToMany - Ta adnotacja oznacza, że pole roles będzie reprezentować relację wiele do wielu. Oznacza to,
+   że jeden użytkownik może mieć wiele ról, a jedna rola może być przypisana do wielu użytkowników.
+
+fetch = FetchType.LAZY - Określa sposób, w jaki dane powinny być pobierane z bazy danych. W tym przypadku,
+kiedy odwołujesz się do pola roles w instancji User, Hibernate (lub inna implementacja JPA) nie będzie
+automatycznie pobierać roli z bazy danych, chyba że zostaną one wyraźnie użyte. Jest to zalecane dla relacji
+typu wiele do wielu, aby unikać ładowania zbędnych danych.
+
+@JoinTable - Jest używane do określenia tabeli pośredniczącej, która będzie przechowywać informacje o relacji
+wiele do wielu między User a Role.
+
+name = "user_roles" - Określa nazwę tabeli pośredniczącej, która przechowuje informacje o relacji wiele
+do wielu między użytkownikami a rolami.
+
+joinColumns - Definiuje kolumnę klucza obcego (user_id), która łączy tabelę użytkowników z tabelą pośredniczącą.
+
+inverseJoinColumns - Definiuje kolumnę klucza obcego (role_id), która łączy tabelę ról z tabelą pośredniczącą.
+
+Podsumowując, ten fragment kodu oznacza, że istnieje relacja wiele do wielu między encją User a encją Role,
+ przechowywana w tabeli pośredniczącej o nazwie user_roles. Ta relacja umożliwia przypisanie wielu ról
+ do wielu użytkowników. */
+
+    public User() {
+    }
 
     public User(String username, String email, String password) {
         this.username = username;
