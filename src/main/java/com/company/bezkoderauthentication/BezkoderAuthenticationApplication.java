c2e152ee935e1c09c88ce5c9945283c6b3cba8cf
magdalenahuget <magdalena.huget@gmail.com>
2023-10-18 11:39:31 +0200

    Initial commit

---
+++ src/main/java/com/company/bezkoderauthentication/BezkoderAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.company.bezkoderauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezkoderAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BezkoderAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/bezkoderauthentication/BezkoderAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.bezkoderauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezkoderAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
