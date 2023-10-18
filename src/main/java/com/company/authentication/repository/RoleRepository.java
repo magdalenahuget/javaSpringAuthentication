2a39f7e5ad44d930690ca0bf23d7fb29077e0c62
magdalenahuget <magdalena.huget@gmail.com>
2023-10-18 13:48:02 +0200

    Add repository implementation

---
+++ src/main/java/com/company/authentication/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.company.authentication.repository;
+
+import com.company.authentication.model.ERole;
+import com.company.authentication.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByUsername(ERole name);
+}
